06a889595b702847d5c95b0e33f2ffb42fdd0246
Maziar Saadatfar <maziar.saadatfar@gmail.com>
2025-10-15 17:31:22 +0330

    Remove the version from config

--- Sources/ContactSupportKit/Service/ContactSupportServiceConfig.swift
+++ Sources/ContactSupportKit/Service/ContactSupportServiceConfig.swift
@@ -8,14 +8,11 @@
 import Foundation
 public struct ContactSupportServiceConfig {
     public init(style: ContactSupportViewStyle = .style1,
-                appId: String,
-                version: String) {
+                appId: String) {
         self.viewConfig = ContactSupportViewStyle.getViewConfigWithStyle(style: style)
         self.appId = appId
-        self.version = version
     }
     public var appId: String
-    public var version: String = Bundle.main.releaseVersionNumber ?? String()
     public var sdkVersion: String = contactSupportKit_Version
     public var viewConfig: ContactSupportViewConfig
 }
